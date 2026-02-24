1ad8cf128adb00e80af6524dcf29c987160832eb
mfini, alfred <alfredmfini@gmail.com>
2026-02-24 16:21:59 +0200

    implementing intermediate project

--- app/main.cc
+++ app/main.cc
@@ -1,5 +1,6 @@
 #include "my_lib.h"
 
+
 int main(int argc, char const *argv[])
 {
     /* code */
--- src/my_lib.cc
+++ src/my_lib.cc
@@ -2,4 +2,5 @@
 
 void print_hello_world (){
     std::cout << "Hello World\n";
+    std::cout << "This is a C++20 project\n";
 }
